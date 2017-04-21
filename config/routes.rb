ef66f9744f6f1fadae7241ad48e4c445b624d0f0
Zachary Ehren <zachehren@gmail.com>
2017-04-20 19:19:11 -0700

    add wiki_controller_spec tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  get "welcome/index"
-
-  get "welcome/about"
+  get "about" => "welcome#about"
 
   root "welcome#index"
 
+  resources :wikis
 end

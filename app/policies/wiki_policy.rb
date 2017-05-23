c3e663c6e478bf2b5f0602740db6eca562c5d6a6
Zachary Ehren <zachehren@gmail.com>
2017-05-22 19:43:08 -0700

    update class Scope authorization

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -30,7 +30,7 @@ class WikiPolicy < ApplicationPolicy
        elsif user.role == 'premium'
          all_wikis = scope.all
          all_wikis.each do |wiki|
-           if !wiki.private? || wiki.user == user || wiki.collaborators.include?(user)
+           if !wiki.private? || wiki.user == user || wiki.users.include?(user)
              wikis << wiki # if the user is premium, only show them public wikis, or that private wikis they created, or private wikis they are a collaborator on
            end
          end
